09288cf606ef07b7610642e9d0d687e9b6571c03
劉敦桐 <zse811022@hotmail.com>
2021-04-19 23:04:06 +0800

    修改留言 修改留言  Edit Reply View

--- Sample_CRUD/Controllers/GuestbooksController.cs
+++ Sample_CRUD/Controllers/GuestbooksController.cs
@@ -38,9 +38,65 @@ namespace Sample_CRUD.Controllers
             //使用Service 來新增一筆資料
             GuestbookService.InsertGuestbooks(Data);
             return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int Id)
+        {
+            //取得頁面所需資料,由Service取得
+            Guestbook Data = GuestbookService.GetDataById(Id);
+            //將資料傳入 View 中
+            return View(Data);
+        }
+        
+        [HttpPost]
+        public ActionResult Edit(int  Id,[Bind(Include ="Name,Content")] Guestbook UpdateData)
+        {
+            if (GuestbookService.CheckUpdate(Id))
+            {
+                //編號設置修改中
+                UpdateData.Id=Id;
+                //使用 Service 來修改資料
+                GuestbookService.UpdateGuestbooks(UpdateData);
+                //重新導向頁面至開始頁面
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        //回覆留言頁面要根據傳入編號來回覆的資料
+        public ActionResult Reply(int Id)
+        {
+            Guestbook Data = GuestbookService.GetDataById(Id);
+
+            return View(Data);
+
+        }
+        [HttpPost]
+        //使用Bind的include來定義接受的欄位，用來避免傳入其他不相干值
+        public ActionResult Reply(int Id, [Bind(Include ="Reply,ReplyTime")] Guestbook ReplyData)
+        {
+            if (GuestbookService.CheckUpdate(Id))
+            {
+                //將編號設定
+                ReplyData.Id = Id;
+                GuestbookService.ReplyGuestbooks(ReplyData);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+
+
+
 
 
         }
+
+
 
     }
 }
